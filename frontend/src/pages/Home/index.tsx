3f1d2e33b8598ddb0a27d7fd93abc0b3428eabec
Phy Lieng <phylieng97@gmail.com>
2021-01-11 06:57:23 +0700

    add react-hook-form

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,10 +1,40 @@
 import React, {useEffect} from 'react';
 import {RouteComponentProps, withRouter} from 'react-router-dom';
+import {useForm} from 'react-hook-form';
 
 const Home = () => {
-    useEffect(() => {
-        console.log("here");
-    }, []);
-    return (<div>Home</div>);
+    const {register, handleSubmit, errors} = useForm();
+    const errorMessage: String = "This field is required";
+    const doublePattern: RegExp = RegExp("[+-]?([0-9]*[.])?[0-9]+");
+    const onSubmit = (data: any) => {
+        console.log(data);
+    };
+
+    return (<form onSubmit={handleSubmit(onSubmit)} id={"pos"}>
+        <div>
+            <label>x</label>
+            <input name={"x"} ref={register({required: true, pattern: doublePattern})} defaultValue={"0"} />
+            {errors.x && errorMessage}
+            <button type={"submit"} form={"pos"}>Save</button>
+
+        </div>
+
+        <div>
+            <label>y</label>
+            <input name={"y"} ref={register({required: true, pattern: doublePattern})} defaultValue={"0"}/>
+
+            {errors.y && errorMessage}
+            <button type={"submit"} form={"pos"}>Save</button>
+
+        </div>
+
+        <div>
+            <label>theta</label>
+            <input name={"theta"} ref={register({required: true, pattern: doublePattern})} defaultValue={"0"}/>
+            {errors.theta && errorMessage}
+            <button type={"submit"} form={"pos"}>Save</button>
+
+        </div>
+    </form>);
 };
 export default withRouter(Home);
